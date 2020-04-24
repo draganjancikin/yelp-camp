177aa1508caffabc917b62836cd7b01bf3709ef3
Dragan Jancikin <dragan.jancikin@gmail.com>
2020-04-24 21:01:49 +0200

    Campground page basic setup: add route to app.js, define temporary campground array and campgrounds.ejs template

--- PROJECT-LOG.md
+++ PROJECT-LOG.md
@@ -1,23 +1,25 @@
 # Project Workflow
 
-- [x] make repository on GitHub
+- [x] Make repository on GitHub
 
-- [x] clone to local directory
+- [x] Clone to local directory
 
-- [x] npm initialize
+- [x] Initialize npm
 ```
 npm init
 ```
 
-- [x] install necesery packages
+- [x] Install necesery packages
 ``` 
 npm i -S express ejs 
 npm i -D nodemon
 ```
 
-- [x] create app.js, make basic express importing, app variable, setting listen port
+- [x] Create app.js, make basic express importing, app variable, setting listen port
 ```
 touch app.js
 ```
 
 - [x] Landing page basic setup: add route to app.js, make view directory and landing.ejs template
+
+- [x] Campground page basic setup: add route to app.js, define temporary campground array and campgrounds.ejs template
--- app.js
+++ app.js
@@ -8,12 +8,28 @@ const port = 3000;
 app.set("view engine", "ejs");
 
 // ==================================================
+// Database
+// ==================================================
+
+
+
+// ==================================================
 // ROUTES
 // ==================================================
 
 // Landing page
 app.get("/", (req, res) => {
   res.render("landing");
+});
+
+// Campgrounds page
+app.get("/campgrounds", (req, res) => {
+  let campgrounds = [
+    { name: "Salmon Creek", image: "https://pixabay.com/get/52e8d4444255ae14f1dc84609620367d1c3ed9e04e50744172287ed0944ac0_340.jpg" },
+    { name: "Granite Hill", image: "https://pixabay.com/get/57e1d14a4e52ae14f1dc84609620367d1c3ed9e04e50744172287ed0944ac0_340.jpg" },
+    { name: "Mountain Goat's Rest", image: "https://pixabay.com/get/52e5d7414355ac14f1dc84609620367d1c3ed9e04e50744172287ed0944ac0_340.jpg" }
+  ];
+  res.render("campgrounds");
 });
 
 // 404 Page
